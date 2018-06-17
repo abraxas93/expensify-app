e042b364e186e7179189323ff7217a4c56f2584c
abraxas93 <savchyk.a@gmail.com>
2018-06-18 01:14:45 +0300

    init commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/style.scss';
+import 'normalize.css/normalize.css'
+
+
+ReactDOM.render(<p>Expensify - app</p>, document.getElementById('app'));
