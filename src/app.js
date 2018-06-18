b863b8ef322375e77d66b752e1d4a075356a7c93
abraxas93 <savchyk.a@gmail.com>
2018-06-18 17:15:25 +0300

    refactored app structure

--- src/app.js
+++ src/app.js
@@ -1,68 +1,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import './styles/style.scss';
 import 'normalize.css/normalize.css';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from my dashboard component
-    </div>
-);
 
-const AddExpensePage = () => (
-    <div>
-        This is from my create expense component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        This is from my edit component
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        This is from my help component
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404 - <Link to="/">Go home</Link>
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create page</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit page</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help page</NavLink>
-    </header>
-);
-
-// /edit 
-// /help
-
-const routes = ( 
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>        
-    </BrowserRouter>
-);
-
-
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create page</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit page</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help page</NavLink>
+    </header>
+);
+
+export default Header;
