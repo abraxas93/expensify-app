a155ee2340dfa08e8874c15760acd7d17db830fc
abraxas93 <savchyk.a@gmail.com>
2018-06-18 15:59:39 +0300

    add navlinks to header

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import './styles/style.scss';
 import 'normalize.css/normalize.css';
 
@@ -13,7 +13,7 @@ const ExpenseDashboardPage = () => (
 
 const AddExpensePage = () => (
     <div>
-        This is from my expense component
+        This is from my create expense component
     </div>
 );
 
@@ -27,7 +27,23 @@ const HelpPage = () => (
     <div>
         This is from my help component
     </div>
-)
+);
+
+const NotFoundPage = () => (
+    <div>
+        404 - <Link to="/">Go home</Link>
+    </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create page</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit page</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help page</NavLink>
+    </header>
+);
 
 // /edit 
 // /help
@@ -35,11 +51,15 @@ const HelpPage = () => (
 const routes = ( 
     <BrowserRouter>
         <div>
-            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-        </div>
+            <Header />
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>        
     </BrowserRouter>
 );
 
