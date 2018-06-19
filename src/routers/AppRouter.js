5142f9d3f1ad89eaa84789ba42be99dc717417b3
abraxas93 <savchyk.a@gmail.com>
2018-06-19 22:45:13 +0300

    basic routing model

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,7 +7,6 @@ import Header from '../components/Header';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 
-
 const AppRouter = () => (
     <BrowserRouter>
         <div>
@@ -15,7 +14,7 @@ const AppRouter = () => (
             <Switch>
                 <Route path="/" component={ExpenseDashboardPage} exact={true}/>
                 <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
