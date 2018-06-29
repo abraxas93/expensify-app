d2ec412267644379682d28d6e9e4867b6149e547
abraxas93 <savchyk.a@gmail.com>
2018-06-29 21:58:31 +0300

    rebuild app structure

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
 import './styles/style.scss';
 import 'normalize.css/normalize.css';
 
+const store = configureStore();
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
