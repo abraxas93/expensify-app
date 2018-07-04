01b5d064284cbb796f1fb2be92bcca7df421ba69
abraxas93 <savchyk.a@gmail.com>
2018-07-04 22:29:25 +0300

    add a select for sortby

--- src/app.js
+++ src/app.js
@@ -13,17 +13,14 @@ import 'normalize.css/normalize.css';
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water Bill', amount: 4000}));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 1500}));
-store.dispatch(addExpense({ description: 'Electricity Bill', amount: 1500}));
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000}));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-//console.log(visibleExpenses);
+console.log(visibleExpenses);
 
-setTimeout(() => {
-    store.dispatch(setTextFilter('gas'));
-}, 3000);
+
 
 const jsx = (
     <Provider store={store}>
