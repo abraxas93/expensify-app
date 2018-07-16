ed09e3c62ebcc06e030f6349730e7132d751850e
abraxas93 <savchyk.a@gmail.com>
2018-07-16 21:07:01 +0300

    fixed bug with expenses

--- src/app.js
+++ src/app.js
@@ -12,10 +12,14 @@ import 'normalize.css/normalize.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4000}));
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4000, createdAt: 0}));
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: -1000}));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 0 }));
 
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -2,12 +2,11 @@ import moment from 'moment';
 // Get visible expenses
 export default (expenses, { text, sortBy, startDate, endDate}) => {
     return expenses.filter((expense) => {
-        const createAtMoment = moment(expense.createAt);
-
+        const createAtMoment = moment(expense.createdAt);
         const startDateMatch = startDate ? startDate.isSameOrBefore(createAtMoment, 'day') : true;
         const endDateMatch = endDate ? endDate.isSameOrAfter(createAtMoment, 'day') : true;
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
-
+        console.log(startDateMatch && endDateMatch && textMatch);
         return startDateMatch && endDateMatch && textMatch;
     }).sort((a, b) => {
         if(sortBy === 'date') {
