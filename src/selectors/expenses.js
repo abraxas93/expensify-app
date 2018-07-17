dbf8afe445162d03052e3be0342a28ca43aad456
abraxas93 <savchyk.a@gmail.com>
2018-07-17 22:38:48 +0300

    fixed bug in expenses selector

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -6,11 +6,10 @@ export default (expenses, { text, sortBy, startDate, endDate}) => {
         const startDateMatch = startDate ? startDate.isSameOrBefore(createAtMoment, 'day') : true;
         const endDateMatch = endDate ? endDate.isSameOrAfter(createAtMoment, 'day') : true;
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
-        console.log(startDateMatch && endDateMatch && textMatch);
         return startDateMatch && endDateMatch && textMatch;
     }).sort((a, b) => {
         if(sortBy === 'date') {
-            return a.createAt < b.createAt ? 1 : -1;
+            return a.createdAt < b.createdAt ? 1 : -1;
         } else if(sortBy === 'amount') {
             return a.amount < b.amount ? 1 : -1;
         }        
